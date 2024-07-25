1fff258829ec2129182a28db1e670813d1df332e
kerbethecoder <krby.cnts@gmail.com>
2024-07-25 15:53:08 +0800

    add workflow
    TODO: modify github pages config

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,7 @@
 	import { openModal } from '../store';
 </script>
 
-<header class="flex flex-col relative z-20 mb-32">
+<header class="flex flex-col relative z-20">
 	<div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
 		<a href="/" class="hover:opacity-60 transition duration-300 ease-in-out">
 			<h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1></a
